27d1ac40e62270e63d6f0145224a6dcdc7fe9f8d
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-08-25 16:26:21 +0000

    reconfiguring the server

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require ('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
+
+app.get('/', (req, res) =>{
+    console.log ("Endpoint reached", req.method)
+    res.sendStatus(200)
+})
+
+app.get('/dashboard', (req, res) =>{
+    console.log("We hit the dashboard input")
+    res.send('hi :3')
+}) 
+
+app.get('/api/data', (req, res)=>{
+    console.log('This is for the data')
+    res.send(data)
+})
 
 app.listen(PORT, () => {
     console.log(`lesbian server running at http://localhost:${PORT}`)
