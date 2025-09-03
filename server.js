c486983d6c5bcc30cc4aaf6b2edf9e48fb2b99ec
Alexandria <alexischloemayer@gmail.com>
2025-09-03 18:48:14 -0500

    Improve dev setup: add watch mode, TypeScript support, SQLite, and env loading

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
-const express = require ('express');
+import express from 'express'
+
+// looks for HTML files, sends them back as a response
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+console.log("remember, we will be watching")
+
+// gets the path to the URL
+const __filename = fileURLToPath(import.meta.url)
+
+// get the directory name
+const __dirname = dirname(__filename)
+
+// serving the HTML file from the public directory
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+
+// waiter waiter! more GIRLS KISSING please!!!
+// middleware
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+/** const express = require ('express');
 const app = express();
 const PORT = 3000;
 
@@ -28,4 +64,4 @@ app.get('/api/data', (req, res)=>{
 
 app.listen(PORT, () => {
     console.log(`lesbian server running at http://localhost:${PORT}`)
-});
+}); **/
